31966975889e77f8203ef98f10091e3a875bcb39
Pritinagi <Pritinagig9494@gmail.com>
2026-07-06 09:07:20 +0530

    Refactor missing value checks using reusable function

--- main.py
+++ main.py
@@ -16,24 +16,44 @@ def count_columns(employees):
     total_columns=len(first_employee)
     return total_columns
 
-def find_missing_first_name(employees):
+# def find_missing_first_name(employees):
+#     count=0
+#     for line in employees:
+#         first_name=line["first_name"]
+#         cleaned_first_name=len(first_name.strip())
+#         if cleaned_first_name==0:
+#             count+=1
+#     return count
+
+def find_missing_values(employees, column_name):
     count=0
     for line in employees:
-        first_name=line["first_name"]
-        cleaned_first_name=len(first_name.strip())
-        if cleaned_first_name==0:
+        column_value=line[column_name]
+        cleaned_column_value=len(column_value.strip())
+        if cleaned_column_value==0:
             count+=1
     return count
 
-
 def main():
+
     employees = load_csv()
     total_rows = count_rows(employees)
     total_columns= count_columns(employees)
-    first_name=find_missing_first_name(employees)
+    # count_missing_first_name=find_missing_first_name(employees)
+    # column_name=input("Enter column name...\n The options you have is : id | first_name | last_name | email | gender | country | joining_date | salary | department | age ")
+    
+
     # print(employees)
     print(f"Total rows present in the current DataSet is : {total_rows}")
     print(f"Total columns present in the current DataSet is : {total_columns}")
-    print(f"Count of Missing First Name  : {first_name}")
+    column_names= [
+        "first_name","last_name","email","gender","joining_date","department"
+        # add the name of columne you want to check out 
+    ]
+    for column_name in column_names:
+        column_missing_values =find_missing_values(employees,column_name )
+        print(f"Count of missing values in {column_name} is {column_missing_values}")
+    # print(f"Count of Missing First Name  : {count_missing_first_name}")
+    # print(f"Count of missing values in {column_name} is {column_missing_values}")
 
 main()
