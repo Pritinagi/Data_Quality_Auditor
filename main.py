790065b682ed19de271bb23c68568f306cb955e5
Pritinagi <Pritinagig9494@gmail.com>
2026-07-06 08:35:57 +0530

    Add missing first name validation

--- main.py
+++ main.py
@@ -16,12 +16,24 @@ def count_columns(employees):
     total_columns=len(first_employee)
     return total_columns
 
+def find_missing_first_name(employees):
+    count=0
+    for line in employees:
+        first_name=line["first_name"]
+        cleaned_first_name=len(first_name.strip())
+        if cleaned_first_name==0:
+            count+=1
+    return count
+
+
 def main():
     employees = load_csv()
     total_rows = count_rows(employees)
     total_columns= count_columns(employees)
+    first_name=find_missing_first_name(employees)
     # print(employees)
     print(f"Total rows present in the current DataSet is : {total_rows}")
     print(f"Total columns present in the current DataSet is : {total_columns}")
+    print(f"Count of Missing First Name  : {first_name}")
 
 main()
