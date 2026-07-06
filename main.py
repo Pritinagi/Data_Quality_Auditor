a0890a01a15e968fbe9e5f331bcdf9cc4830f2bc
Pritinagi <Pritinagig9494@gmail.com>
2026-07-06 21:27:46 +0530

    Add age validation with exception handling

--- main.py
+++ main.py
@@ -26,16 +26,16 @@ def count_columns(employees):
 #     return count
 
 def find_missing_values(employees, column_name):
-    count=0
+    count_missing_row=0
     for line in employees:
         column_value=line[column_name]
         cleaned_column_value=len(column_value.strip())
         if cleaned_column_value==0:
-            count+=1
-    return count
+            count_missing_row+=1
+    return count_missing_row
 
 def find_invalid_emails(employees):
-    count =0
+    count_invalid_email =0
     for line in employees:
         email=line["email"]
         value_error_1=email.count('@')
@@ -43,20 +43,42 @@ def find_invalid_emails(employees):
         if cleaned_email==0:
             continue      
         elif '@' not in email:
-             count+=1
+             count_invalid_email+=1
         elif value_error_1>1 :
-            count+=1
+            count_invalid_email+=1
         else:
             parts = email.split("@")
             part_1=len(parts[0])
             part_2=len(parts[1])
             part_2_value=parts[1].count(".")
             if part_1==0 or part_2==0:
-                count+=1    
+                count_invalid_email+=1    
             elif part_2_value<1:
-                count+=1       
-    return count
+                count_invalid_email+=1       
+    return count_invalid_email
 
+
+def find_invalid_age(employees):
+    count_invalid_age=0
+    for line in employees:
+        age=line["age"]
+        cleaned_age=len(age.strip())
+        if cleaned_age==0:
+            continue
+        try:
+            int_age=int(age)
+        except ValueError:
+            count_invalid_age+=1
+        else :
+            if int_age < 18 or int_age > 65:
+                count_invalid_age += 1
+
+        # it is an alternative
+        # if age.isnumeric():
+        #     int_age=int(line["age"])
+        #     if int_age < 18 or int_age > 65:
+        #         count_invalid_age += 1
+    return count_invalid_age
 
 
 def main():
@@ -67,7 +89,7 @@ def main():
     total_invalid_emails=find_invalid_emails(employees)
     # count_missing_first_name=find_missing_first_name(employees)
     # column_name=input("Enter column name...\n The options you have is : id | first_name | last_name | email | gender | country | joining_date | salary | department | age ")
-    
+    total_invalid_age= find_invalid_age(employees)
 
     # print(employees)
     print(f"Total rows present in the current DataSet is : {total_rows}")
@@ -82,5 +104,7 @@ def main():
     # print(f"Count of Missing First Name  : {count_missing_first_name}")
     # print(f"Count of missing values in {column_name} is {column_missing_values}")
     print(f"Count of invalid emails {total_invalid_emails} ")
+    print(f"Count of invalid age {total_invalid_age} ")
+
 
 main()
